0f9f0b3f101e8a787e8f4e071c01ec4931464662
Nikita Barinov <nikitabarinov2301@gmail.com>
2026-08-17 15:12:12 +0400

    feat(cincin): add prefersReducedMotion to public api

--- packages/cincin/src/dom/index.ts
+++ packages/cincin/src/dom/index.ts
@@ -1,3 +1,4 @@
 export { attachSwipe } from './attach-swipe';
 export type { SwipeOptions } from './attach-swipe';
 export type { SwipeDirection } from './gesture';
+export { prefersReducedMotion } from './utils';
